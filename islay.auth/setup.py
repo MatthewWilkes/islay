3de90cc22a67e93a088f84076a92602cedcbac45
Matthew Wilkes <matthew@matthewwilkes.co.uk>
2010-07-22 00:43:17 +0200

    Update documentation

--- islay.auth/setup.py
+++ islay.auth/setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = '0.1'
 
 setup(name='islay.auth',
       version=version,
       description="An unobtrusive authentication framework for WSGI stacks.",
       long_description=open(os.path.join('src', 'islay', 'auth', 'README.txt')).read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        "Topic :: Security",
         ],
-      keywords='',
+      keywords='wsgi python authentication repoze.who',
       author='Matthew Wilkes',
       author_email='wilkes@jarn.com',
-      url='',
-      license='',
+      url='http://code.google.com/p/islay/',
+      license='Modified BSD',
       package_dir = {'':'src'},
       packages=find_packages('src', exclude=['ez_setup']),
       namespace_packages=['islay'],
