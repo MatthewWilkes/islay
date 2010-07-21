64145bb2be9de711a09f8d99c533aa80a8ab865e
Matthew Wilkes <matthew@matthewwilkes.co.uk>
2010-07-21 22:05:15 +0200

    A WSGI app that emits forbidden responses and a test to set it up and check it does.

--- islay.auth/src/islay/auth/tests/base.py
+++ islay.auth/src/islay/auth/tests/base.py
@@ -2,3 +2,7 @@ import unittest
 
 class IslayAuthTestCase(unittest.TestCase):
     pass
+
+def ForbiddenApp(environ, start_response):
+    start_response(403, [])
+    return
--- islay.auth/src/islay/auth/tests/test_forbidden.py
+++ islay.auth/src/islay/auth/tests/test_forbidden.py
@@ -1,7 +1,23 @@
+import webob
+
+from islay.auth.auth import AuthFactory
+
 from islay.auth.tests.base import IslayAuthTestCase
+from islay.auth.tests.base import ForbiddenApp
 
 class TestForbidden(IslayAuthTestCase):
     
-    def test_one(self):
-        self.failUnless(True)
-
+    def setUp(self):
+        e={'HTTP_HOST':'example', 'wsgi.url_scheme':'http'}
+        self.request = webob.Request(e)
+        self.forbidden = ForbiddenApp
+        self.app = AuthFactory({})(self.forbidden)
+    
+    def test_unwrapped_application_returns_403(self):
+        response = self.request.get_response(self.forbidden)
+        self.assertEqual(response.status, '403 Forbidden')
+    
+    def test_middleware_hides_403(self):
+        response = self.request.get_response(self.forbidden)
+        self.failIf(response.status.startswith('40'), '40x status code')
+    
