78027c67071b63891eaa29914144bb14edf5b53c
Matthew Wilkes <matthew@matthewwilkes.co.uk>
2010-07-21 22:35:02 +0200

    Use unauthorised rather than forbidden everywhere

--- islay.auth/src/islay/auth/tests/base.py
+++ islay.auth/src/islay/auth/tests/base.py
@@ -1,11 +1,34 @@
 import unittest
+from zope.interface import implements
+
+from islay.auth.interfaces import IChallenger
 
 class IslayAuthTestCase(unittest.TestCase):
     pass
 
-def ForbiddenApp(environ, start_response):
+# Applications that mimic certain aspects of the authorisation dance
+
+def UnauthorisedApp(environ, start_response):
     headers = []
     headers.append(('WWW-Authenticate', 'Basic realm="WSGI"'))
 
     start_response(401, headers)
     return
+
+def ForbiddenApp(environ, start_response):
+    headers = []
+    start_response(403, headers)
+    return
+
+
+# Fake plugins we can use to make life easy
+
+class StaticTextChallenger(object):
+    implements(IChallenger)
+    
+    def challenge(self, environ, status, app_headers, forget_headers):
+        def ChallengeApp(environ, start_response):
+            headers = [("Content-type", "text/plain"), ]
+            start_response(200, headers)
+            return ["Who do you think you are?", ]
+        return ChallengeApp
--- islay.auth/src/islay/auth/tests/test_forbidden.py
+++ islay.auth/src/islay/auth/tests/test_forbidden.py
@@ -4,19 +4,38 @@ from islay.auth.auth import AuthFactory
 
 from islay.auth.tests.base import IslayAuthTestCase
 from islay.auth.tests.base import ForbiddenApp
+from islay.auth.tests.base import UnauthorisedApp
 
-class TestForbidden(IslayAuthTestCase):
+class TestUnauthorised(IslayAuthTestCase):
     
     def setUp(self):
         e={'HTTP_HOST':'example', 'wsgi.url_scheme':'http'}
         self.request = webob.Request(e)
-        self.forbidden = ForbiddenApp
-        self.app = AuthFactory({})(self.forbidden)
+        self.unauthorised = UnauthorisedApp
+        self.app = AuthFactory({})(self.unauthorised)
     
     def test_unwrapped_application_returns_401(self):
-        response = self.request.get_response(self.forbidden)
+        response = self.request.get_response(self.unauthorised)
         self.assertEqual(response.status, '401 Unauthorized')
     
     def test_middleware_hides_401(self):
-        response = self.request.get_response(self.forbidden)
+        response = self.request.get_response(self.unauthorised)
         self.failIf(response.status.startswith('40'), '40x status code')
+
+class TestChallenger(IslayAuthTestCase):
+    
+    def setUp(self):
+        e={'HTTP_HOST':'example', 'wsgi.url_scheme':'http'}
+        self.request = webob.Request(e)
+        self.unauthorised = UnauthorisedApp
+        self.forbidden = ForbiddenApp
+        self.app = AuthFactory({}, challenger='islay.auth.tests.base.StaticTextChallenger')(self.unauthorised)
+    
+    def test_challenger_is_invoked_on_401(self):
+        response = self.request.get_response(self.unauthorised)
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.body, 'Who do you think you are?')
+    
+    def test_challenger_passes_403(self):
+        response = self.request.get_response(self.forbidden)
+        self.assertEqual(response.status, '403 Forbidden')
