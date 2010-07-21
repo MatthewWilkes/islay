f0c302ef94cfdaddaadd0d3d0343485d32e1a5df
Matthew Wilkes <matthew@matthewwilkes.co.uk>
2010-07-21 21:52:55 +0200

    Testcase setup

---
+++ islay.auth/src/islay/auth/tests/base.py
@@ -0,0 +1,4 @@
+import unittest
+
+class IslayAuthTestCase(unittest.TestCase):
+    pass
---
+++ islay.auth/src/islay/auth/tests/test_forbidden.py
@@ -0,0 +1,7 @@
+from islay.auth.tests.base import IslayAuthTestCase
+
+class TestForbidden(IslayAuthTestCase):
+    
+    def test_one(self):
+        self.failUnless(True)
+
