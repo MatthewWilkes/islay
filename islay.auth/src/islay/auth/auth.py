41cee2770137beb3392eee0cb1714683184b3d75
Matthew Wilkes <matthew@matthewwilkes.co.uk>
2010-07-22 23:22:27 +0200

    Move to using an inner lambda for the constructor and fix a scoping error. Also fix a silly syntax error.

--- islay.auth/src/islay/auth/auth.py
+++ islay.auth/src/islay/auth/auth.py
@@ -40,71 +40,79 @@ def AuthFactory(global_config, **local_conf):
     
     for path in local_conf.get('identifier', '').split(','):
         if path:
-            identifiers.append(importFromName(path))()
+            identifiers.append(importFromName(path)())
 
     for path in local_conf.get('authenticator', '').split(','):
         if path:
-            authenticators.append(importFromName(path))()
+            authenticators.append(importFromName(path)())
 
     for path in local_conf.get('challenger', '').split(','):
         if path:
-            challengers.append(importFromName(path))()
+            challengers.append(importFromName(path)())
+    
+    return lambda app: AuthMiddleware(app, identifiers, authenticators, challengers)
 
     
-    class AuthMiddleware(object):
-        """An endpoint"""
-    
-        def __init__(self, app):
-            self.app = app
-            self.identifiers = identifiers
-            self.authenticators = authenticators
-            self.challengers = challengers            
-    
-        def __call__(self, environ, start_response):
-            request = Request(environ)
+class AuthMiddleware(object):
+    """An endpoint"""
 
-            auth = None
-            identifier = None
-
-            for identifier in self.identifiers:
-                identifier = identifier
-                credentials = identifier.identify(environ)
-                if credentials is None:
-                    continue
-                else:
-                    for authenticator in authenticators:
-                        auth = authenticator.authenticate(environ, credentials)
-                        if auth is None:
-                            continue
-                        else:
-                            break
+    def __init__(self, app, identifiers=None, authenticators=None, challengers=None):
+        self.app = app
+        
+        if identifiers is None:
+            identifiers = []
             
-            if auth:
-                request.environ['REMOTE_USER'] = auth
-            elif 'REMOTE_USER' in request.environ:
-                del request.environ['REMOTE_USER']
+        if authenticators is None:
+            authenticators = []
             
-            response = request.get_response(self.app)
+        if challengers is None:
+            challengers = []
+        
+        self.identifiers = identifiers
+        self.authenticators = authenticators
+        self.challengers = challengers            
 
-            if response.status == '401 Unauthorized':
-                for challenger in self.challengers:
-                    if identifier is not None:
-                        forget_headers = identifier.forget(environ, credentials)
-                    else:
-                        forget_headers = []
-                    challenge = challenger.challenge(environ, 
-                                                     response.status, 
-                                                     response.headers, 
-                                                     forget_headers)
-                    if challenge is None:
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+
+        auth = None
+        identifier = None
+
+        for identifier in self.identifiers:
+            identifier = identifier
+            credentials = identifier.identify(environ)
+            if credentials is None:
+                continue
+            else:
+                for authenticator in self.authenticators:
+                    auth = authenticator.authenticate(environ, credentials)
+                    if auth is None:
                         continue
                     else:
-                        return challenge(environ, start_response)
-            else:
-                if identifier is not None:
-                    response.headers.update(identifier.remember(environ, credentials))
-                return response(environ, start_response)
-    
-    return AuthMiddleware
+                        break
+        
+        if auth:
+            request.environ['REMOTE_USER'] = auth
+        elif 'REMOTE_USER' in request.environ:
+            del request.environ['REMOTE_USER']
+        
+        response = request.get_response(self.app)
 
-AuthMiddleware = AuthFactory({}) # Generic version without config
+        if response.status == '401 Unauthorized':
+            for challenger in self.challengers:
+                if identifier is not None:
+                    forget_headers = identifier.forget(environ, credentials)
+                else:
+                    forget_headers = []
+                challenge = challenger.challenge(environ, 
+                                                 response.status, 
+                                                 response.headers, 
+                                                 forget_headers)
+                if challenge is None:
+                    continue
+                else:
+                    return challenge(environ, start_response)
+        else:
+            if identifier is not None:
+                response.headers.update(identifier.remember(environ, credentials))
+            return response(environ, start_response)
